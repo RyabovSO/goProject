5f56dec8f78071d4e47a5371a9aa3ec695a91799
RyabovSO <ryabov.ser.o@gmail.com>
2018-07-22 17:29:54 +0300

    create new project

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "index", nil)
+}
+
+func writeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	t.ExecuteTemplate(w, "write", nil)
+}
+
+
+func main() {
+	fmt.Println("Listening on port :3000")
+
+	// /css/app.css
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/write", writeHandler)
+	//http.HandleFunc("/saveNode", saveNodeHandler)
+
+	http.ListenAndServe(":3000", nil)
+}
